9d32edcca7999b59671d70f24dbc36c99011f66c
jdmedlock <jdmedlock@gmail.com>
2018-08-05 07:50:54 -0500

    Feature: Add basic functionality
    Add basic functionality for `on`, `notify`, and `trigger` functions
    
    Resolves: N/a
    See also: N/a

--- events.js
+++ events.js
@@ -20,5 +20,54 @@
 //
 // nephewParties.trigger( 'mainEvent', 'ice cream' );
 //
+// SPECIAL CONSIDERATIONS:
+// Since the Udacity quiz grading system seems to sometimes have problems
+// with ES6 syntax (as of August 2018) this app is written in ES5. 
+//
 
+function EventTracker(eventName) {
+  this.eventName = eventName;
+  this.events = [];
+}
 
+EventTracker.prototype.on = function (eventName, callback) {
+  console.log('Entered "on"');
+  this.events.push( {eventName: eventName, callback: callback} );
+}
+
+EventTracker.prototype.notify = function (evtTracker, eventName) {
+  console.log('Entered "notify"');
+  evtTracker.trigger(eventName, null);
+}
+
+EventTracker.prototype.trigger = function (eventName, callbackParms) {
+  console.log('Entered "trigger"');
+  var event = this.events.find(function(entry) {
+    return entry.eventName === eventName;
+  });
+  console.log('event: ', event);
+  event.callback(callbackParms);
+}
+
+// Test the functionality of the EventTracker system
+function purchase(item) { 
+  console.log( 'purchasing ' + item);
+}
+
+function celebrate() {
+  console.log( this.name + ' says birthday parties are awesome!' );
+}
+
+var nephewParties = new EventTracker( 'nephews ');
+var richard = new EventTracker( 'Richard' );
+
+nephewParties.on( 'mainEvent', purchase );
+richard.on( 'mainEvent', celebrate );
+nephewParties.notify( richard, 'mainEvent' );
+
+nephewParties.trigger( 'mainEvent', 'ice cream' );
+
+// Log the attributes for each instance of the EventTracker class to
+// aid in debugging
+console.log('newphewParties events: ', nephewParties.events);
+console.log('richard events: ', richard.events);
